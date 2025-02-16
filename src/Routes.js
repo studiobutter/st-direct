521e9c7e340d356bb538ee9ac579fb75f6a2f454
Tạ Nguyên Khang <55838591+studiobutter@users.noreply.github.com>
2025-02-16 19:03:02 +0700

    Update Routes.js - Replace HashRouter with BrowserRouter

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // Main Page
 import Home from './pages/home';
